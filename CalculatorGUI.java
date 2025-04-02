f53db423c0c9e9fdb145721929be9f7c4852ec40
bera2003 <anuragbera7@gmail.com>
2025-04-02 11:04:02 +0530

    taking frame variables

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -2,3 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+public class CalculatorGUI {
+    private JFrame frame;
+    private JTextField textField1, textField2, resultField;
+    private JComboBox<String> operationBox;
