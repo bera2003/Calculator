382b35858d9819619b20ec0e2e0839588e024b7b
bera2003 <anuragbera7@gmail.com>
2025-04-02 11:05:42 +0530

    fixed some lines

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -7,4 +7,10 @@ public class CalculatorGUI {
     private JFrame frame;
     private JTextField textField1, textField2, resultField;
     private JComboBox<String> operationBox;
+
+    public CalculatorGUI() {
+        frame = new JFrame("Calculator");
+        frame.setSize(350, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(5, 2));
 }
